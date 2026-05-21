3b3cca0b1ddb0b4744d762d37ea9e924064d5fd1
William Goodson <goodsonwill@gmail.com>
2026-05-21 08:09:45 -0400

    Improved extract script

--- README.md
+++ README.md
@@ -3,9 +3,15 @@
 ## Overview
 This project consists of extracting reviews from movie rating sites into a database, integrating an LLM to pull the actual sentiment from a review, and displaying analytics. Data will be pulled on request through a web interface. The user will be able to search for a movie, and if that movie is available it will display the appropriate analytic dashboard. Otherwise, it will find and extract the new movie. The goal of this project is to showcase my capabilites in building and designing data systems and pipelines.
 
+I want to be able to track my prorgress and challenges, so I am documenting my progress as I go along in the Timeline & Changelog section of this document.
+
 ## Design
 
-![Design](./design.jpeg "Design")
+### Initial Design
+![Initial Design](./docs/images/design.jpeg "Initial Design")
+
+### Database Schema
+![Database Schema](./docs/images/schema.png "Database Schema")
 
 ## Application Stack
 
@@ -16,3 +22,19 @@ This project consists of extracting reviews from movie rating sites into a datab
 **Database:** PostgreSQL
 
 **API Framework:** FastAPI
+
+## Timeline & Changelog
+
+### Initial commit with added extraction
+At this stage, I have the basic extraction script done. It pulls movie details and reviews from the web, and imports them into a postgres database. Still needs some refinement, but it works! The database schema has deviated from the initial design. It has tables for actors, directors, genres, reviews, and movies as well as a table to link actors to movies, since movies have more than one actor and actors can be in more than one movie.
+
+---
+
+### Improved extract script
+The script was running into issues and was difficult to troubleshoot without messages. To prevent the script from being blocked, I utilized the **curl_cffi** library to immitate a web browser and use sessions. I also implemented a random wait to more closely replicate human behavior. For the actual data, I found that some movies have multiple genres and directors, so I reworked those in a smilar fashion to the actors. So now there is are two additional tables: genremovie and directormovie. Tags will probably follow a similar structure when I add them later. Lastly, I have improved data validation and created better logging for errors. I have created a dashboard in tableau to visualize some of the data with a few movies I have added.
+
+![Vis](./docs/images/commit-2-vis.png)
+
+---
+
+
--- scripts/extract.py
+++ scripts/extract.py
@@ -8,6 +8,8 @@ import re
 import hashlib
 import os
 import argparse
+from curl_cffi import requests
+import random
 
 ## Load Environment Variables
 load_dotenv()
@@ -46,8 +48,6 @@ class Movies(BaseModel):
     description = TextField()
     release_year = IntegerField()
     status = CharField()
-    director = ForeignKeyField(Directors, backref='directs')
-    genre = ForeignKeyField(Genres, backref='movies')
 
 class ActorMovie(BaseModel):
     actor = ForeignKeyField(Actors, backref='movies_link')
@@ -55,6 +55,22 @@ class ActorMovie(BaseModel):
     class Meta():
         indexes = (
             (('actor', 'movie'), True),
+        )
+
+class DirectorMovie(BaseModel):
+    director = ForeignKeyField(Directors, backref='movies_link')
+    movie = ForeignKeyField(Movies, backref='directors_link')
+    class Meta():
+        indexes = (
+            (('director', 'movie'), True),
+        )
+
+class GenreMovie(BaseModel):
+    genre = ForeignKeyField(Genres, backref='movies_link')
+    movie = ForeignKeyField(Movies, backref='genres_link')
+    class Meta():
+        indexes = (
+            (('genre', 'movie'), True),
         )
 
 class Reviews(BaseModel):
@@ -70,83 +86,137 @@ class Reviews(BaseModel):
         )
 
 HEADERS = {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) ",
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
                 "Accept-Language": "en-US,en;q=0.9",
-                "Sec-Fetch-Site": "none",
-                "Sec-Fetch-Mode": "navigate",
-                "Sec-Fetch-Dest": "document",
             }
 
 ## Initialize Database
 db.connect()
-db.create_tables([Directors, Actors, Genres, Movies, ActorMovie, Reviews])
+db.create_tables([Directors, Actors, Genres, Movies, Reviews, ActorMovie, DirectorMovie, GenreMovie])
 
 def extract_reviews(movie_slug, movie_id):
     """Takes in the movie slug with the movie id. Scrapes reviews from letterboxd using pagination. Then loads the reviews into the database."""
     REVIEW_LIST = set()
     page = 1
+    session = requests.Session(impersonate="chrome")
+    session.headers.update(HEADERS)
+    print('Scraping reviews...')
+    start_time = time.time()
     while True:
-        print(f'Scraping page {page}...')
-        html = req.get(f'https://letterboxd.com/film/{movie_slug}/reviews/by/added-earliest/page/{page}', headers=HEADERS)
-        soup = BeautifulSoup(html.text, 'html.parser')
+        res = session.get(f'https://letterboxd.com/film/{movie_slug}/reviews/by/added-earliest/page/{page}')
+        if res.status_code != 200:
+            print('Script is being blocked!')
+            break
+        soup = BeautifulSoup(res.text, 'html.parser')
         reviews = soup.find_all("article", class_='production-viewing')
         for review in reviews:
             try:
+                r_time = dt.datetime.strptime(str(review.find('time').text).strip(), "%d %b %Y")
+            except Exception as err:
+                print(f'Failed to convert time for review on page {page}!', err, (str(review.find('time').text).strip()))
+                continue
+            try:
                 review_text = str(review.find('div', class_='body-text').text).strip()
                 REVIEW_LIST.add((
-                    dt.datetime.strptime(str(review.find('time').text).strip(), "%d %b %Y"),
+                    r_time,
                     process_rating(review.find('title')),
                     review_text,
                     hashlib.md5(review_text.encode('utf-8')).hexdigest()))
             except Exception as err:
-                print(err)
+                print('Failed to add review!', err)
         if len(reviews) < 12:
             break
+        session.headers.update({"Referer": "https://letterboxd.com/film/{movie_slug}/reviews/page/{page}"})
         page=page+1
         # Please dont rate limit me
-        time.sleep(0.25)
+        time.sleep(random.uniform(3.0, 7.0))
 
     review_ins = [{'post_time': p_time, 'rating_score': score, 'full_review': review, 'movie': movie_id, 'sentiment_score': 0, 'review_hash': hash} for p_time, score, review, hash in REVIEW_LIST]
     Reviews.insert_many(review_ins).on_conflict_ignore().execute()
+    print('Done. Runtime: ', time.time() - start_time, 'seconds')
 
 def extract_movie(movie_slug):
     """Takes movie slug from url and looks up movie and scrapes page for movie data before inserting data into the database and returning the movie id"""
-    html = req.get(f'https://letterboxd.com/film/{movie_slug}/')
+    html = req.get(f'https://letterboxd.com/film/{movie_slug}/', headers=HEADERS)
     soup = BeautifulSoup(html.text, 'html.parser')
-    # Description
-    desc = soup.find('section', class_='production-synopsis')
-    desc = desc.find('p').text
-    # Actors
-    actors = soup.find('div', class_='cast-list')
-    actors = [actor.text for actor in actors.find_all('a')]
-    # Genre
-    genre = soup.find('div', id='tab-panel-genres')
-    genre = genre.find('a').text
+    #Field Defaults
+    actors = []
+    genres = []
+    directors = []
+    desc = ''
+    name = ''
+    release_year = ''
+    start_time = time.time()
+    print('Extracting Movie Info...')
     # Movie Details
-    details = soup.find('div', class_='details')
-    name = details.find('span', class_='name').text
-    release_year = details.find('span', class_='releasedate').text
-    director = details.find_all('a', class_='contributor')[0].text
-    print(movie_slug, name, desc, release_year, director, genre, actors)
-
-    # Insert Genre
-    genre_obj, created = Genres.get_or_create(name=genre)
-    # Insert Director
-    director_obj, created = Directors.get_or_create(name=director)
+    try:
+        details = soup.find('div', class_='details')
+        try:
+            name = details.find('span', class_='name').text
+        except Exception as err:
+            print('Could not find movie name!', err)
+            return None
+        try:
+            release_year = details.find('span', class_='releasedate').text
+        except Exception as err:
+            print('Could not find release year!', err)
+            return None
+        try:
+            directors = [director.text for director in details.find_all('a', class_='contributor')]
+        except Exception as err:
+            print('Could not find directors!', err)
+            return None
+    except Exception as err:
+        print('Could not find details!', err)
+        return None
+    # Description
+    try:
+        desc = soup.find('section', class_='production-synopsis')
+        desc = desc.find('p').text
+    except Exception as err:
+        print('Could not find description!', err)
+        return None
+    # Actors
+    try:
+        actors = soup.find('div', class_='cast-list')
+        actors = [actor.text for actor in actors.find_all('a')]
+        try:
+            actors.remove('Show All…')
+        except:
+            pass
+    except Exception as err:
+        print('Could not find actors!', err)
+        return None
+    # Genres
+    try:
+        genres = soup.find('div', id='tab-panel-genres')
+        genres = genres.find_next('div')
+        genres = [genre.text for genre in genres.find_all('a')]
+    except Exception as err:
+        print('Could not find genres!', err)
+        return None
     # Insert Movie
-    movie_obj, created = Movies.get_or_create(slug=movie_slug,
-                              name=name,
-                              description=desc,
-                              release_year=release_year,
-                              status='',
-                              director=director_obj.id,
-                              genre=genre_obj.id)
+    movie_obj, created = Movies.get_or_create(
+                            slug=movie_slug,
+                            name=name,
+                            description=desc,
+                            release_year=release_year,
+                            status='')
     # Insert Actors and Populate ActorMovie
     for actor in actors:
         actor_obj, created = Actors.get_or_create(name=actor)
         ActorMovie.insert({'actor': actor_obj.id, 'movie': movie_obj.id}).on_conflict_ignore().execute()
+     # Insert Genres and Populate GenreMovie
+    for genre in genres:
+        genre_obj, created = Genres.get_or_create(name=genre)
+        GenreMovie.insert({'genre': genre_obj.id, 'movie': movie_obj.id}).on_conflict_ignore().execute()
+    # Insert Director and Populate DirectorMovie
+    for director in directors:
+        director_obj, created = Directors.get_or_create(name=director)
+        DirectorMovie.insert({'director': director_obj.id, 'movie': movie_obj.id}).on_conflict_ignore().execute()
 
+    print('Done. Runtime: ', time.time()-start_time, 'seconds')
     return movie_obj.id
 
 
@@ -170,10 +240,16 @@ def process_link(link:str):
         print('Invalid URL! Enter Letterboxd movie URL of format https://letterboxd.com/film/(movie)/')
         return None
     slug = link.split('/')[4]
+    time.sleep(1)
     return slug
 
-slug = process_link(args.url)
-if slug:
-    id = extract_movie(slug)
-    extract_reviews(slug, id)
-    print('done')
+def main ():
+    slug = process_link(args.url)
+    if slug:
+        id = extract_movie(slug)
+        if id:
+            extract_reviews(slug, id)
+            print('done')
+
+if __name__ == "__main__":
+    main()
